f15605bbaf3840ba7f91b309f7df49bf64908a0e
Manan Bhatt <manan16489@gmail.com>
2022-09-06 10:12:49 +0530

    sample

--- src/test/java/io/orkes/conductor/client/Examples.java
+++ src/test/java/io/orkes/conductor/client/Examples.java
@@ -12,12 +12,17 @@
  */
 package io.orkes.conductor.client;
 
+import io.orkes.conductor.client.http.model.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import io.orkes.conductor.client.http.ApiClient;
 import io.orkes.conductor.client.http.api.*;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Examples {
 
@@ -49,26 +54,91 @@ public class Examples {
         authorizationResourceApi = new AuthorizationResourceApi(apiClient);
     }
 
-    @BeforeEach
-    public void setup() {}
-
-    @Test
-    @DisplayName("tag a user and group")
-    public void tagUserAndGroup() {}
-
     @Test
     @DisplayName("tag a workflows and task")
-    public void tagWorkflowsAndTasks() {}
+    public void tagWorkflowsAndTasks() {
+        // Create workflow definition
+        String taskName = "trye";
+        String workflowName = "Workflow_klioqa";
+        WorkflowDef workflowDef = getWorkflowDef(workflowName, taskName);
+        // Create workflow definition.
+        metadataResourceApi.create(workflowDef, true);
+
+        // Create task definition
+        TaskDef taskDef = new TaskDef();
+        taskDef.setName(taskName);
+        metadataResourceApi.registerTaskDef(Arrays.asList(taskDef));
+
+        // Tag a task
+        TagObject tagObject = new TagObject();
+        tagObject.setKey("department");
+        tagObject.setValue("account");
+        tagsApi.addTaskTag(tagObject, taskName);
+
+
+        // Tag a workflow
+        tagsApi.addWorkflowTag(tagObject, workflowName);
+    }
 
     @Test
     @DisplayName("add auth to tags")
-    public void addAuthToTags() {}
+    public void addAuthToTags() {
+        //Create a tag
+        TagObject tagObject = new TagObject();
+        tagObject.setKey("department");
+        tagObject.setValue("HR");
+
+        //Add auth to tags
+        AuthorizationRequest authorizationRequest = new AuthorizationRequest();
+        authorizationRequest.access(Arrays.asList(AuthorizationRequest.AccessEnum.EXECUTE));
+//        authorizationResourceApi.grantPermissions();
+    }
 
     @Test
     @DisplayName("create workflow definition")
-    public void createWorkflowDef() {}
+    public void createWorkflowDef() {
+        // Create workflow definition
+        String workflowName = "Workflow_tweytey";
+        String taskName = "Task_wyuw";
+        WorkflowDef workflowDef = getWorkflowDef(workflowName, taskName);
+        metadataResourceApi.create(workflowDef, true);
+
+        // Create task definition
+        TaskDef taskDef = new TaskDef();
+        taskDef.setName(taskName);
+        metadataResourceApi.registerTaskDef(Arrays.asList(taskDef));
+    }
 
     @Test
     @DisplayName("start workflow")
-    public void startWorkflow() {}
+    public void startWorkflow() {
+        // Create workflow definition
+        String workflowName = "Workflow_wtyet";
+        String taskName = "Task_wyuwew";
+        WorkflowDef workflowDef = getWorkflowDef(workflowName, taskName);
+        metadataResourceApi.create(workflowDef, true);
+
+        // Create task definition
+        TaskDef taskDef = new TaskDef();
+        taskDef.setName(taskName);
+        metadataResourceApi.registerTaskDef(Arrays.asList(taskDef));
+        StartWorkflowRequest startWorkflowRequest = new StartWorkflowRequest();
+        startWorkflowRequest.setName(workflowName);
+        startWorkflowRequest.setVersion(1);
+        Map<String, Object> input = new HashMap<>();
+        startWorkflowRequest.setInput(input);
+        workflowResourceApi.startWorkflow(startWorkflowRequest);
+    }
+
+
+    WorkflowDef getWorkflowDef(String workflowName, String taskName) {
+        WorkflowDef workflowDef = new WorkflowDef();
+        workflowDef.setName(workflowName);
+        workflowDef.setVersion(1);
+        workflowDef.setOwnerEmail("manan.bhatt@orkes.io");
+        WorkflowTask workflowTask = new WorkflowTask();
+        workflowTask.setName(taskName);
+        workflowDef.setTasks(Arrays.asList(workflowTask));
+        return workflowDef;
+    }
 }
