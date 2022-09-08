1962426e6a8ec466ea1640de216c2d1a254fa084
Viren Baraiya <virenx@gmail.com>
2022-09-07 23:21:18 -0500

    more clients

--- src/main/java/io/orkes/conductor/client/http/OrkesMetadataClient.java
+++ src/main/java/io/orkes/conductor/client/http/OrkesMetadataClient.java
@@ -1,9 +1,32 @@
 package io.orkes.conductor.client.http;
 
+import com.netflix.conductor.client.config.ConductorClientConfiguration;
 import com.netflix.conductor.client.http.MetadataClient;
+import com.sun.jersey.api.client.ClientHandler;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.filter.ClientFilter;
 import io.orkes.conductor.client.http.auth.AuthorizationClientFilter;
 
 public class OrkesMetadataClient extends MetadataClient implements OrkesClient {
+    public OrkesMetadataClient() {
+    }
+
+    public OrkesMetadataClient(ClientConfig clientConfig) {
+        super(clientConfig);
+    }
+
+    public OrkesMetadataClient(ClientConfig clientConfig, ClientHandler clientHandler) {
+        super(clientConfig, clientHandler);
+    }
+
+    public OrkesMetadataClient(ClientConfig config, ClientHandler handler, ClientFilter... filters) {
+        super(config, handler, filters);
+    }
+
+    public OrkesMetadataClient(ClientConfig config, ConductorClientConfiguration clientConfiguration, ClientHandler handler, ClientFilter... filters) {
+        super(config, clientConfiguration, handler, filters);
+    }
+
     @Override
     public void withCredentials(String keyId, String secret) {
         this.client.addFilter(new AuthorizationClientFilter(root, keyId, secret));
