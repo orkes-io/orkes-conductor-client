7a8fe272e4e47521240d5d5907ed4a666197dd9c
gardusig <gustavo.gardusi@gmail.com>
2022-09-12 23:36:36 -0300

    Added tests for worfklow client

--- src/test/java/io/orkes/conductor/client/api/WorkflowClientTests.java
+++ src/test/java/io/orkes/conductor/client/api/WorkflowClientTests.java
@@ -18,14 +18,17 @@ import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 
+import com.netflix.conductor.common.metadata.workflow.RerunWorkflowRequest;
 import com.netflix.conductor.common.metadata.workflow.StartWorkflowRequest;
 import com.netflix.conductor.common.run.Workflow;
 
 import io.orkes.conductor.client.WorkflowClient;
+import io.orkes.conductor.client.http.ApiException;
 import io.orkes.conductor.client.util.Commons;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class WorkflowClientTests extends ClientTest {
     private final WorkflowClient workflowClient;
@@ -70,16 +73,33 @@ public class WorkflowClientTests extends ClientTest {
                 () -> {
                     workflowClient.searchV2(Commons.WORKFLOW_NAME);
                 });
+        assertThrows(
+                UnsupportedOperationException.class,
+                () -> {
+                    workflowClient.searchV2(0, 0, "", "", "");
+                });
         assertNotNull(workflowClient.getWorkflows(Commons.WORKFLOW_NAME, "abc", false, false));
         workflowClient.terminateWorkflow(workflowId, "reason");
         workflowClient.retryLastFailedTask(workflowId);
+        workflowClient.getRunningWorkflow(Commons.WORKFLOW_NAME, Commons.WORKFLOW_VERSION);
+        workflowClient.getWorkflowsByTimePeriod(
+                Commons.WORKFLOW_NAME, Commons.WORKFLOW_VERSION, 0L, 0L);
+        workflowClient.search(2, 5, "", "", Commons.WORKFLOW_NAME);
         workflowClient.terminateWorkflows(List.of(workflowId), "reason");
         workflowClient.restart(workflowId, true);
         workflowClient.terminateWorkflow(List.of(workflowId), "reason");
         workflowClient.restartWorkflow(List.of(workflowId), true);
-        workflowClient.getRunningWorkflow(Commons.WORKFLOW_NAME, Commons.WORKFLOW_VERSION);
+        workflowClient.terminateWorkflow(workflowId, "reason");
+        workflowClient.retryWorkflow(List.of(workflowId));
+        workflowClient.terminateWorkflow(workflowId, "reason");
+        workflowClient.rerunWorkflow(workflowId, new RerunWorkflowRequest());
         workflowClient.pauseWorkflow(workflowId);
         workflowClient.resumeWorkflow(workflowId);
+        try {
+            workflowClient.skipTaskFromWorkflow(workflowId, Commons.TASK_NAME);
+        } catch (ApiException e) {
+            assertTrue(e.getMessage().contains("cannot be skipped"));
+        }
         workflowClient.pauseWorkflow(List.of(workflowId));
         workflowClient.resumeWorkflow(List.of(workflowId));
         workflowClient.deleteWorkflow(workflowId, false);
