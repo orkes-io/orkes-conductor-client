252b556631c79f31d6723bef97d46a2b38f95749
gardusig <gustavo.gardusi@gmail.com>
2022-09-16 18:44:50 -0300

    Added tests for scheduler client

--- src/test/java/io/orkes/conductor/client/api/SchedulerClientTests.java
+++ src/test/java/io/orkes/conductor/client/api/SchedulerClientTests.java
@@ -15,8 +15,16 @@ package io.orkes.conductor.client.api;
 import org.junit.jupiter.api.Test;
 
 import io.orkes.conductor.client.SchedulerClient;
+import io.orkes.conductor.client.model.SaveScheduleRequest;
+import io.orkes.conductor.client.model.WorkflowSchedule;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SchedulerClientTests extends ClientTest {
+    private final String NAME = "test_sdk_java_scheduler_name";
+    private final String CRON_EXPRESSION = "0 * * * * *";
 
     private final SchedulerClient schedulerClient;
 
@@ -25,5 +33,34 @@ public class SchedulerClientTests extends ClientTest {
     }
 
     @Test
-    void testMethods() {}
+    void testMethods() {
+        schedulerClient.deleteSchedule(NAME);
+        assertTrue(schedulerClient.getNextFewSchedules(CRON_EXPRESSION, 0L, 0L, 0).isEmpty());
+        schedulerClient.saveSchedule(getSaveScheduleRequest());
+        WorkflowSchedule workflowSchedule = schedulerClient.getSchedule(NAME);
+        assertEquals(NAME, workflowSchedule.getName());
+        assertEquals(CRON_EXPRESSION, workflowSchedule.getCronExpression());
+        assertFalse(schedulerClient.searchV22(0, 10, "ASC", "*", "").getResults().isEmpty());
+        schedulerClient.pauseSchedule(NAME);
+        workflowSchedule = schedulerClient.getSchedule(NAME);
+        assertTrue(workflowSchedule.isPaused());
+        schedulerClient.resumeSchedule(NAME);
+        workflowSchedule = schedulerClient.getSchedule(NAME);
+        assertFalse(workflowSchedule.isPaused());
+        schedulerClient.deleteSchedule(NAME);
+    }
+
+    @Test
+    void testDebugMethods() {
+        schedulerClient.pauseAllSchedules();
+        schedulerClient.resumeAllSchedules();
+        schedulerClient.requeueAllExecutionRecords();
+    }
+
+    SaveScheduleRequest getSaveScheduleRequest() {
+        SaveScheduleRequest saveScheduleRequest = new SaveScheduleRequest();
+        saveScheduleRequest.setName(NAME);
+        saveScheduleRequest.setCronExpression(CRON_EXPRESSION);
+        return saveScheduleRequest;
+    }
 }
