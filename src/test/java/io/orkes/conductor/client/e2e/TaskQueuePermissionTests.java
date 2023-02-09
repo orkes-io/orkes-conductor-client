c7d21d78296571ed9d40bfa8a34e83266bb827a3
gardusig <gustavo.gardusi@gmail.com>
2023-02-09 14:45:20 -0300

    cleanup

--- src/test/java/io/orkes/conductor/client/e2e/TaskQueuePermissionTests.java
+++ src/test/java/io/orkes/conductor/client/e2e/TaskQueuePermissionTests.java
@@ -3,43 +3,71 @@ package io.orkes.conductor.client.e2e;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.junit.Assert;
 
 import com.netflix.conductor.common.metadata.workflow.StartWorkflowRequest;
 
+import io.orkes.conductor.client.ApiClient;
+import io.orkes.conductor.client.AuthorizationClient;
 import io.orkes.conductor.client.MetadataClient;
+import io.orkes.conductor.client.OrkesClients;
 import io.orkes.conductor.client.TaskClient;
 import io.orkes.conductor.client.WorkflowClient;
+import io.orkes.conductor.client.api.ClientTest;
+import io.orkes.conductor.client.http.ApiException;
 import io.orkes.conductor.client.http.OrkesTaskClient;
+import io.orkes.conductor.client.model.ConductorApplication;
+import io.orkes.conductor.client.model.CreateOrUpdateApplicationRequest;
 import io.orkes.conductor.client.model.UpsertGroupRequest.RolesEnum;
+import io.orkes.conductor.client.util.ApiUtil;
 import io.orkes.conductor.client.util.RegistrationUtil;
-import io.orkes.conductor.client.util.authentication.TestWithAuthentication;
 
-public class TaskQueuePermissionTests extends TestWithAuthentication {
+public class TaskQueuePermissionTests extends ClientTest {
 
     private static final int RETRY_LIMIT = 5;
 
     private final WorkflowClient workflowClient;
     private final MetadataClient metadataClient;
+    private final AuthorizationClient authorizationClient;
+
+    private final List<ConductorApplication> createdApplicationsToDelete;
 
     public TaskQueuePermissionTests() {
-        workflowClient = orkesClients.getWorkflowClient();
-        metadataClient = orkesClients.getMetadataClient();
+        workflowClient = super.orkesClients.getWorkflowClient();
+        metadataClient = super.orkesClients.getMetadataClient();
+        authorizationClient = super.orkesClients.getAuthorizationClient();
+        createdApplicationsToDelete = new ArrayList<>();
+    }
+
+    @AfterEach
+    public void cleanup() {
+        createdApplicationsToDelete.forEach(
+                application -> {
+                    authorizationClient.deleteApplication(application.getId());
+                    Assert.assertThrows(
+                            ApiException.class,
+                            () -> {
+                                authorizationClient.getApplication(application.getId());
+                            });
+                });
+        createdApplicationsToDelete.clear();
     }
 
     @Test
     public void testTaskQueuePermissionForUserWithPreAuthorizedRole() {
-        var adminOrkesClients = getOrkesClientsWithRole(RolesEnum.ADMIN);
-        var taskClient = adminOrkesClients.getTaskClient();
-
+        var application = generateApplication(RolesEnum.USER);
         var workflowName = generateRandomStringWithPrefix("random_workflow_name_");
         var taskName = generateRandomStringWithPrefix("random_task_name_");
-
         var workflowId = startRandomWorkflow(workflowName, taskName);
+        var taskClient = getOrkesClients(application).getTaskClient();
         waitUntilTaskIsPresentAtTaskQueue(taskClient, taskName, true);
-
         workflowClient.deleteWorkflow(workflowId, false);
         waitUntilTaskIsPresentAtTaskQueue(taskClient, taskName, false);
     }
@@ -76,5 +104,30 @@ public class TaskQueuePermissionTests extends TestWithAuthentication {
         startWorkflowRequest.setName(workflowName);
         startWorkflowRequest.setVersion(1);
         return workflowClient.startWorkflow(startWorkflowRequest);
+    }
+
+    private ConductorApplication generateApplication(RolesEnum role) {
+        var applicationName = generateRandomStringWithPrefix(
+                "random_application_name_");
+        var request = new CreateOrUpdateApplicationRequest().name(applicationName);
+        var application = authorizationClient.createApplication(request);
+        assertNotNull(application);
+        this.createdApplicationsToDelete.add(application);
+        authorizationClient.addRoleToApplicationUser(
+                application.getId(), role.getValue());
+        return application;
+    }
+
+    private OrkesClients getOrkesClients(ConductorApplication application) {
+        var accessKeyResponse = authorizationClient.createAccessKey(application.getId());
+        var apiClient = new ApiClient(
+                ApiUtil.getBasePath(),
+                accessKeyResponse.getId(),
+                accessKeyResponse.getSecret());
+        return new OrkesClients(apiClient);
+    }
+
+    private String generateRandomStringWithPrefix(String prefix) {
+        return prefix + UUID.randomUUID().toString();
     }
 }
