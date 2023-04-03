eacd9bffbaeace98d864976293e50748b9004042
manan164 <1897158+manan164@users.noreply.github.com>
2023-04-03 22:57:30 +0530

    more changes

--- src/test/java/io/orkes/conductor/client/e2e/WorkerTaskPermissionTests.java
+++ src/test/java/io/orkes/conductor/client/e2e/WorkerTaskPermissionTests.java
@@ -70,8 +70,8 @@ public class WorkerTaskPermissionTests {
         }
         // Create group and add these two users in the group
         Group group = authorizationClient.upsertGroup(getUpsertGroupRequest(), groupName);
-        authorizationClient.addUserToGroup(groupName, "user1@orkes.io");
-        authorizationClient.addUserToGroup(groupName, "user2@orkes.io");
+        authorizationClient.addUserToGroup(groupName, "conductoruser1@gmail.com");
+        authorizationClient.addUserToGroup(groupName, "conductoruser2@gmail.com");
 
         // Give permissions to tag in the group
         AuthorizationRequest authorizationRequest = new AuthorizationRequest();
