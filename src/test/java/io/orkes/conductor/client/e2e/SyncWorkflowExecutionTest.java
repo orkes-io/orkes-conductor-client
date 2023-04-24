43830f95ccfcaa9b1a1fece7e17613604f097eb7
manan164 <1897158+manan164@users.noreply.github.com>
2023-04-24 22:25:25 +0530

    dead code

--- src/test/java/io/orkes/conductor/client/e2e/SyncWorkflowExecutionTest.java
+++ src/test/java/io/orkes/conductor/client/e2e/SyncWorkflowExecutionTest.java
@@ -12,14 +12,7 @@
  */
 package io.orkes.conductor.client.e2e;
 
-import com.netflix.conductor.common.metadata.tasks.Task;
-import com.netflix.conductor.common.metadata.tasks.TaskDef;
-import com.netflix.conductor.common.metadata.tasks.TaskResult;
-import com.netflix.conductor.common.metadata.tasks.TaskType;
-import com.netflix.conductor.common.metadata.workflow.RerunWorkflowRequest;
 import com.netflix.conductor.common.metadata.workflow.StartWorkflowRequest;
-import com.netflix.conductor.common.metadata.workflow.WorkflowDef;
-import com.netflix.conductor.common.metadata.workflow.WorkflowTask;
 import com.netflix.conductor.common.run.Workflow;
 import io.orkes.conductor.client.ApiClient;
 import io.orkes.conductor.client.MetadataClient;
@@ -28,28 +21,20 @@ import io.orkes.conductor.client.WorkflowClient;
 import io.orkes.conductor.client.http.OrkesMetadataClient;
 import io.orkes.conductor.client.http.OrkesTaskClient;
 import io.orkes.conductor.client.http.OrkesWorkflowClient;
-import io.orkes.conductor.client.model.WorkflowStatus;
 import io.orkes.conductor.client.util.ApiUtil;
 import io.orkes.conductor.common.model.WorkflowRun;
-import io.orkes.conductor.proto.WorkflowRunPb;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.time.Duration;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
-import static io.orkes.conductor.client.util.RegistrationUtil.registerWorkflowDef;
-import static io.orkes.conductor.client.util.RegistrationUtil.registerWorkflowWithSubWorkflowDef;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.testcontainers.shaded.org.awaitility.Awaitility.await;
 
 public class SyncWorkflowExecutionTest {
 
