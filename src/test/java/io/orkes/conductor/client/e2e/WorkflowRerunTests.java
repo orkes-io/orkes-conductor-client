8fc1d9117d4604770d5837038c891e84235535c5
manan164 <1897158+manan164@users.noreply.github.com>
2023-04-27 22:28:47 +0530

    e2e for fork rerun validation

--- src/test/java/io/orkes/conductor/client/e2e/WorkflowRerunTests.java
+++ src/test/java/io/orkes/conductor/client/e2e/WorkflowRerunTests.java
@@ -13,9 +13,14 @@
 package io.orkes.conductor.client.e2e;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
+import com.netflix.conductor.common.metadata.tasks.TaskDef;
+import com.netflix.conductor.common.metadata.tasks.TaskType;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.jupiter.api.BeforeAll;
@@ -208,5 +213,118 @@ public class WorkflowRerunTests {
             metadataClient.unregisterWorkflowDef(workflowName, 1);
             metadataClient.unregisterTaskDef(taskName);
         } catch (Exception e){}
+    }
+
+    @Test
+    @DisplayName("Check workflow fork join task rerun")
+    public void testRerunForkJoinWorkflow() {
+
+        String workflowName = "re-run-fork-workflow";
+        // Register workflow
+        registerForkJoinWorkflowDef(workflowName, metadataClient);
+        workflowNames.add(workflowName);
+
+        // Trigger two workflows
+        StartWorkflowRequest startWorkflowRequest = new StartWorkflowRequest();
+        startWorkflowRequest.setName(workflowName);
+        startWorkflowRequest.setVersion(1);
+
+        String workflowId = workflowClient.startWorkflow(startWorkflowRequest);
+        Workflow workflow = workflowClient.getWorkflow(workflowId, true);
+        assertEquals(3, workflow.getTasks().size());
+        // Fail the simple task
+        String taskId = workflow.getTasks().get(2).getTaskId();
+        TaskResult taskResult = new TaskResult();
+        taskResult.setWorkflowInstanceId(workflowId);
+        taskResult.setTaskId(taskId);
+        taskResult.setStatus(TaskResult.Status.COMPLETED);
+        taskClient.updateTask(taskResult);
+
+        taskId = workflow.getTasks().get(1).getTaskId();
+        taskResult = new TaskResult();
+        taskResult.setWorkflowInstanceId(workflowId);
+        taskResult.setTaskId(taskId);
+        taskResult.setStatus(TaskResult.Status.FAILED_WITH_TERMINAL_ERROR);
+        taskClient.updateTask(taskResult);
+
+        // Wait for workflow to get failed
+        await().atMost(1, TimeUnit.SECONDS).untilAsserted(() -> {
+            Workflow workflow1 = workflowClient.getWorkflow(workflowId, false);
+            assertEquals(workflow1.getStatus().name(), WorkflowStatus.StatusEnum.FAILED.name());
+        });
+
+        RerunWorkflowRequest rerunWorkflowRequest = new RerunWorkflowRequest();
+        rerunWorkflowRequest.setReRunFromWorkflowId(workflowId);
+        rerunWorkflowRequest.setReRunFromTaskId(taskId);
+        // Retry the workflow
+        workflowClient.rerunWorkflow(workflowId, rerunWorkflowRequest);
+        // Check the workflow status and few other parameters
+        await().atMost(5, TimeUnit.SECONDS).untilAsserted(() -> {
+            Workflow workflow1 = workflowClient.getWorkflow(workflowId, true);
+            assertEquals(workflow1.getStatus().name(), WorkflowStatus.StatusEnum.RUNNING.name());
+            assertEquals(workflow1.getTasks().get(2).getStatus().name(), Task.Status.SCHEDULED.name());
+            // Rerun should not affect this task since it was already completed.
+            assertEquals(workflow1.getTasks().get(1).getStatus().name(), Task.Status.COMPLETED.name());
+        });
+
+        taskResult = new TaskResult();
+        taskResult.setWorkflowInstanceId(workflowId);
+        taskResult.setTaskId(taskId);
+        taskResult.setStatus(TaskResult.Status.COMPLETED);
+        taskClient.updateTask(taskResult);
+        workflowClient.runDecider(workflowId);
+        System.out.println("Going to check workflow " + workflowId + " for completion");
+
+        // Wait for workflow to get completed
+        await().atMost(30, TimeUnit.SECONDS).untilAsserted(() -> {
+            Workflow workflow1 = workflowClient.getWorkflow(workflowId, false);
+            assertEquals(WorkflowStatus.StatusEnum.COMPLETED.name(), workflow1.getStatus().name(), "Workflow " + workflowId + " did not complete");
+        });
+
+        try {
+            metadataClient.unregisterWorkflowDef(workflowName, 1);
+        }catch (Exception e){}
+    }
+
+    private void registerForkJoinWorkflowDef(String workflowName, MetadataClient metadataClient) {
+
+        String fork_task = RandomStringUtils.randomAlphanumeric(5).toUpperCase();
+        String fork_task1 = RandomStringUtils.randomAlphanumeric(5).toUpperCase();
+        String fork_task2 = RandomStringUtils.randomAlphanumeric(5).toUpperCase();
+        String join_task = RandomStringUtils.randomAlphanumeric(5).toUpperCase();
+
+        WorkflowTask fork_task_1 = new WorkflowTask();
+        fork_task_1.setTaskReferenceName(fork_task1);
+        fork_task_1.setName(fork_task1);
+        fork_task_1.setWorkflowTaskType(TaskType.SIMPLE);
+
+        WorkflowTask fork_task_2 = new WorkflowTask();
+        fork_task_2.setTaskReferenceName(fork_task2);
+        fork_task_2.setName(fork_task2);
+        fork_task_2.setWorkflowTaskType(TaskType.SIMPLE);
+
+
+        WorkflowTask fork_workflow_task = new WorkflowTask();
+        fork_workflow_task.setTaskReferenceName(fork_task);
+        fork_workflow_task.setName(fork_task);
+        fork_workflow_task.setWorkflowTaskType(TaskType.SIMPLE);
+        fork_workflow_task.setForkTasks(List.of(List.of(fork_task_1), List.of(fork_task_2)));
+
+        WorkflowTask join = new WorkflowTask();
+        join.setTaskReferenceName(join_task);
+        join.setName(join_task);
+        join.setWorkflowTaskType(TaskType.JOIN);
+        join.setJoinOn(List.of("fork_task_1", "fork_task_2"));
+
+
+        WorkflowDef workflowDef = new WorkflowDef();
+        workflowDef.setName(workflowName);
+        workflowDef.setInputParameters(Arrays.asList("value", "inlineValue"));
+        workflowDef.setDescription("Workflow to test retry");
+        workflowDef.setTimeoutSeconds(600);
+        workflowDef.setTimeoutPolicy(WorkflowDef.TimeoutPolicy.TIME_OUT_WF);
+        workflowDef.setTasks(Arrays.asList(fork_workflow_task, join));
+        workflowDef.setOwnerEmail("test@orkes.io");
+        metadataClient.registerWorkflowDef(workflowDef);
     }
 }
