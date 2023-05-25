1f9696b6aca6ba9c6bc052b94cd697f2fa99a119
supriyanta <supriyanta@supriyantas-MacBook-Air.local>
2023-05-25 21:22:37 +0530

    Add Kitchen-sink work in e2e test

--- src/test/java/io/orkes/conductor/client/e2e/JavaSDKTests.java
+++ src/test/java/io/orkes/conductor/client/e2e/JavaSDKTests.java
@@ -13,14 +13,20 @@
 package io.orkes.conductor.client.e2e;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
+import com.netflix.conductor.common.metadata.tasks.TaskDef;
+import com.netflix.conductor.common.metadata.tasks.TaskType;
+import com.netflix.conductor.common.metadata.workflow.WorkflowDef;
+import com.netflix.conductor.common.metadata.workflow.WorkflowTask;
+import com.netflix.conductor.sdk.workflow.def.tasks.*;
+import com.netflix.conductor.sdk.workflow.task.InputParam;
 import org.junit.After;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import com.netflix.conductor.client.http.MetadataClient;
@@ -28,8 +34,6 @@ import com.netflix.conductor.client.http.TaskClient;
 import com.netflix.conductor.client.http.WorkflowClient;
 import com.netflix.conductor.common.run.Workflow;
 import com.netflix.conductor.sdk.workflow.def.ConductorWorkflow;
-import com.netflix.conductor.sdk.workflow.def.tasks.SimpleTask;
-import com.netflix.conductor.sdk.workflow.def.tasks.Switch;
 import com.netflix.conductor.sdk.workflow.executor.WorkflowExecutor;
 import com.netflix.conductor.sdk.workflow.task.WorkerTask;
 
@@ -43,6 +47,19 @@ public class JavaSDKTests {
 
     private WorkflowExecutor executor;
 
+    private static ApiClient apiClient;
+    private static TaskClient taskClient;
+    private static WorkflowClient workflowClient;
+    private static MetadataClient metadataClient;
+
+    @BeforeAll
+    public static void init() {
+        apiClient = ApiUtil.getApiClientWithCredentials();
+        taskClient = new OrkesClients(apiClient).getTaskClient();
+        workflowClient = new OrkesClients(apiClient).getWorkflowClient();
+        metadataClient = new OrkesClients(apiClient).getMetadataClient();
+    }
+
     @Test
     public void hello() {
         ConductorWorkflow<Map<String, Object>> workflow = new ConductorWorkflow<>(executor);
@@ -51,12 +68,9 @@ public class JavaSDKTests {
         workflow.setVariables(new HashMap<>());
 
     }
+
     @Test
     public void testSDK() throws ExecutionException, InterruptedException, TimeoutException {
-        ApiClient apiClient = ApiUtil.getApiClientWithCredentials();
-        TaskClient taskClient = new OrkesClients(apiClient).getTaskClient();
-        WorkflowClient workflowClient = new OrkesClients(apiClient).getWorkflowClient();
-        MetadataClient metadataClient = new OrkesClients(apiClient).getMetadataClient();
         executor = new WorkflowExecutor(taskClient, workflowClient, metadataClient, 1000);
         executor.initWorkers("io.orkes.conductor.client.e2e");
 
@@ -67,12 +81,6 @@ public class JavaSDKTests {
         workflow.setVariables(new HashMap<>());
         workflow.add(new SimpleTask("task1", "task1").input("name", "orkes"));
 
-        Switch decision = new Switch("decide_ref", "${workflow.input.caseValue}");
-        decision.switchCase("caseA", new SimpleTask("task1", "task1"), new SimpleTask("task1", "task11"));
-        decision.switchCase("caseB", new SimpleTask("task2", "task2"));
-        decision.defaultCase(new SimpleTask("task1", "default_task"));
-
-
         CompletableFuture<Workflow> future = workflow.executeDynamic(new HashMap<>());
         assertNotNull(future);
         Workflow run = future.get(20, TimeUnit.SECONDS);
@@ -82,11 +90,142 @@ public class JavaSDKTests {
         assertEquals("Hello, orkes", run.getTasks().get(0).getOutputData().get("greetings"));
     }
 
+    @Test
+    public void testKitchenSinkWorkflow0()
+            throws ExecutionException, InterruptedException, TimeoutException {
+        ConductorWorkflow<Map<String, Object>> workflow = createWorkflow(0);
+
+        CompletableFuture<Workflow> future = workflow.executeDynamic(Map.of(
+                "task2Name", "task_5",
+                "mod", 2,
+                "oddEven", 5
+        ));
+
+        assertNotNull(future);
+
+        Workflow run = future.get(60, TimeUnit.SECONDS);
+        assertNotNull(run);
+
+        for (com.netflix.conductor.common.metadata.tasks.Task task : run.getTasks()) {
+            assertEquals(com.netflix.conductor.common.metadata.tasks.Task.Status.COMPLETED, task.getStatus());
+        }
+
+        assertEquals(Workflow.WorkflowStatus.COMPLETED, run.getStatus());
+        assertNull(run.getReasonForIncompletion());
+        assertEquals(8, run.getTasks().size());
+    }
+
+    @Test
+    public void testKitchenSinkWorkflow1()
+            throws ExecutionException, InterruptedException, TimeoutException {
+        ConductorWorkflow<Map<String, Object>> workflow = createWorkflow(1);
+
+        CompletableFuture<Workflow> future = workflow.executeDynamic(Map.of(
+                "task2Name", "task_5",
+                "mod", 2,
+                "oddEven", 5
+        ));
+
+        assertNotNull(future);
+
+        Workflow run = future.get(60, TimeUnit.SECONDS);
+        assertNotNull(run);
+
+        for (com.netflix.conductor.common.metadata.tasks.Task task : run.getTasks()) {
+            assertEquals(com.netflix.conductor.common.metadata.tasks.Task.Status.COMPLETED, task.getStatus());
+        }
+
+        assertEquals(Workflow.WorkflowStatus.COMPLETED, run.getStatus());
+        assertNull(run.getReasonForIncompletion());
+        assertEquals(10, run.getTasks().size());
+    }
+
     @After
     public void cleanup() {
-        if(executor != null) {
+        if (executor != null) {
             executor.shutdown();
         }
+    }
+
+    @WorkerTask("task_1_1")
+    public Map<String, Object> task_1_1() {
+        return Map.of(
+                "mod", 5,
+                "oddEven", 0
+        );
+    }
+
+    @WorkerTask("task_4")
+    public Map<String, Object> task_4(
+            @InputParam("mod") String mod,
+            @InputParam("oddEven") String oddEven
+    ) {
+        return Map.of(
+                "mod", 5,
+                "taskToExecute", "dynamic_task",
+                "oddEven", 0,
+                "dynamicTasks", List.of(
+                        Map.of(
+                                "name", "task_1",
+                                "taskReferenceName", "task_1_1",
+                                "type", "SIMPLE"
+                        ),
+                        Map.of(
+                                "name", "wf_dyn",
+                                "taskReferenceName", "wf_dyn",
+                                "type", "SUB_WORKFLOW",
+                                "subWorkflowParam", Map.of(
+                                        "name", "sub_flow_1"
+                                )
+                        )
+                ),
+                "dynamicTasksInput", Map.of(
+                        "task_1_1", Map.of(),
+                        "wf_dyn", Map.of()
+                )
+        );
+    }
+
+    @WorkerTask("task_1")
+    public Map<String, Object> task_1(
+            @InputParam("mod") String mod,
+            @InputParam("oddEven") String oddEven
+    ) {
+        return Map.of(
+                "mod", 5,
+                "taskToExecute", "dynamic_task",
+                "oddEven", 1,
+                "dynamicTasks", List.of(
+                        Map.of(
+                                "name", "task_1",
+                                "taskReferenceName", "task_1_1",
+                                "type", "SIMPLE"
+                        ),
+                        Map.of(
+                                "name", "sub_workflow_4",
+                                "taskReferenceName", "wf_dyn",
+                                "type", "SUB_WORKFLOW",
+                                "subWorkflowParam", Map.of(
+                                        "name", "sub_flow_1"
+                                )
+                        )
+                ),
+                "inputs", Map.of(
+                        "task_1_1", Map.of(),
+                        "wf_dyn", Map.of()
+                )
+        );
+    }
+
+    @WorkerTask("dynamic_task")
+    public Map<String, Object> dynamicTask(
+            @InputParam("flow") int flow
+    ) {
+        int oddEven = flow;
+        return Map.of(
+                "mod", 5,
+                "oddEven", oddEven
+        );
     }
 
     @WorkerTask("sum_numbers")
@@ -94,4 +233,108 @@ public class JavaSDKTests {
         return num1.add(num2);
     }
 
+    private void registerDefinitions() {
+        TaskDef task1TaskDef = new TaskDef();
+        task1TaskDef.setOwnerEmail("test@orkes.io");
+        task1TaskDef.setName("dynamic_task");
+
+        TaskDef task1_1TaskDef = new TaskDef();
+        task1_1TaskDef.setOwnerEmail("test@orkes.io");
+        task1_1TaskDef.setName("task_1_1");
+        metadataClient.registerTaskDefs(List.of(task1TaskDef, task1_1TaskDef));
+
+        WorkflowTask subWorkflowTask = new WorkflowTask();
+        subWorkflowTask.setWorkflowTaskType(TaskType.SIMPLE);
+        subWorkflowTask.setName("task_1");
+        subWorkflowTask.setTaskReferenceName("task_1");
+
+        WorkflowDef subWorkflowDef = new WorkflowDef();
+        subWorkflowDef.setName("sub_flow_1");
+        subWorkflowDef.setOwnerEmail("test@orkes.io");
+        subWorkflowDef.setDescription("sub flow 1");
+        subWorkflowDef.setTasks(List.of(subWorkflowTask));
+        metadataClient.registerWorkflowDef(subWorkflowDef);
+    }
+
+    private ConductorWorkflow<Map<String, Object>> createWorkflow(int flow) {
+        registerDefinitions();
+
+        executor = new WorkflowExecutor(taskClient, workflowClient, metadataClient, 1000);
+        executor.initWorkers("io.orkes.conductor.client.e2e");
+
+        ConductorWorkflow<Map<String, Object>> workflow = new ConductorWorkflow<>(executor);
+        workflow.setName("kitchensink");
+        workflow.setVersion(1);
+        workflow.setOwnerEmail("test@orkes.io");
+        workflow.setVariables(new HashMap<>());
+
+        workflow.add(
+                new SimpleTask("task_1", "task_1")
+                        .input(
+                                Map.of(
+                                        "mod", "${workflow.input.mod}",
+                                        "oddEven", "${workflow.input.oddEven}"
+                                )
+                        )
+        );
+
+        workflow.add(
+                new Dynamic("task_2", "taskToExecute")
+                        .input(Map.of(
+                                "taskToExecute", "${task_1.output.taskToExecute}",
+                                "flow", flow
+                        ))
+        );
+
+        workflow.add(
+                new Switch("oddEvenDecision", "${task_2.output.oddEven}")
+                        .input(Map.of("switchCaseValue", "${task_2.output.oddEven}"))
+                        .switchCase(
+                                "0",
+                                new SimpleTask("task_4", "task_4")
+                                        .input(
+                                                Map.of(
+                                                        "mod", "${task_2.output.mod}",
+                                                        "oddEven", "${task_2.output.oddEven}"
+                                                )
+                                        ),
+                                new DynamicFork("fanout1", "forkedTasks", "forkedTasksInputs")
+                                        .input(
+                                                Map.of(
+                                                        "forkedTasks", "${task_4.output.dynamicTasks}",
+                                                        "forkedTasksInputs", "${task_4.output.dynamicTasksInput}"
+                                                )
+                                        )
+                        )
+                        .switchCase(
+                                "1",
+                                new ForkJoin(
+                                        "forkx",
+                                        new Task[]{
+                                                new SimpleTask("task_1", "task_10"),
+                                                new SubWorkflow("wf3", "sub_flow_1", 1)
+                                                        .input(
+                                                        Map.of(
+                                                                "mod", "${task_1.output.mod}",
+                                                                "oddEven", "${task_1.output.oddEven}"
+                                                        )
+                                                )
+                                        },
+                                        new Task[]{
+                                                new SimpleTask("task_1", "task_11"),
+                                                new SubWorkflow("wf4", "sub_flow_1", 1)
+                                                        .input(
+                                                        Map.of(
+                                                                "mod", "${task_1.output.mod}",
+                                                                "oddEven", "${task_1.output.oddEven}"
+                                                        )
+                                                )
+                                        }
+                                ),
+                                new Join("join2", "wf3", "wf4")
+                        )
+        );
+
+        return workflow;
+    }
 }
