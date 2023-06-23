fde60e8bbf9f6aa3f48ac9c5100c46c507ab0eba
gardusig <gustavo.gardusi@gmail.com>
2023-06-23 19:23:58 -0300

    Added support for void methods at testUtil

--- src/test/java/io/orkes/conductor/client/util/TestUtil.java
+++ src/test/java/io/orkes/conductor/client/util/TestUtil.java
@@ -3,6 +3,24 @@ package io.orkes.conductor.client.util;
 public class TestUtil {
     private static int RETRY_ATTEMPT_LIMIT = 3;
 
+    public static void retryMethodCall(VoidRunnableWithException function)
+            throws Exception {
+        for (int retryCounter = 0; retryCounter < RETRY_ATTEMPT_LIMIT; retryCounter += 1) {
+            try {
+                function.run();
+                return;
+            } catch (Exception e) {
+                System.out.println("Attempt " + (retryCounter + 1) + " failed: " + e.getMessage());
+                try {
+                    Thread.sleep(1000 * (1 << retryCounter)); // Sleep for 2^retryCounter second(s) before retrying
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+        throw new Exception("Failed to execute method call after " + RETRY_ATTEMPT_LIMIT + " attempts");
+    }
+
     public static Object retryMethodCall(RunnableWithException function)
             throws Exception {
         for (int retryCounter = 0; retryCounter < RETRY_ATTEMPT_LIMIT; retryCounter += 1) {
@@ -23,5 +41,10 @@ public class TestUtil {
     @FunctionalInterface
     public interface RunnableWithException {
         Object run() throws Exception;
+    }
+
+    @FunctionalInterface
+    public interface VoidRunnableWithException {
+        void run() throws Exception;
     }
 }
