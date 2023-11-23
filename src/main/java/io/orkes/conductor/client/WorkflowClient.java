a7448f74e3fcfc86a29fbecb054da19a3efa805c
manan164 <1897158+manan164@users.noreply.github.com>
2023-11-23 21:41:30 +0530

    changes

--- src/main/java/io/orkes/conductor/client/WorkflowClient.java
+++ src/main/java/io/orkes/conductor/client/WorkflowClient.java
@@ -19,6 +19,7 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
+import com.netflix.conductor.common.metadata.workflow.IdempotencyStrategy;
 import com.netflix.conductor.common.metadata.workflow.StartWorkflowRequest;
 import com.netflix.conductor.common.metadata.workflow.UpgradeWorkflowRequest;
 import com.netflix.conductor.common.model.BulkResponse;
@@ -38,6 +39,8 @@ public abstract class WorkflowClient extends com.netflix.conductor.client.http.W
     public abstract CompletableFuture<WorkflowRun> executeWorkflow(StartWorkflowRequest request, String waitUntilTask, Integer waitForSeconds);
 
     public abstract WorkflowRun executeWorkflow(StartWorkflowRequest request, String waitUntilTask, Duration waitTimeout) throws ExecutionException, InterruptedException, TimeoutException;
+    public abstract CompletableFuture<WorkflowRun> executeWorkflow(StartWorkflowRequest request, String waitUntilTask, Integer waitForSeconds, String xIdempotencyKey, IdempotencyStrategy xOnConflict );
+    public abstract WorkflowRun executeWorkflow(StartWorkflowRequest request, String waitUntilTask, Duration waitTimeout, String xIdempotencyKey, IdempotencyStrategy xOnConflict) throws ExecutionException, InterruptedException, TimeoutException;
 
     public abstract BulkResponse pauseWorkflow(List<String> workflowIds) throws ApiException;
 
